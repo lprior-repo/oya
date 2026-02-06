c3559fff8aaf216ee2bd5fe6b18028ad6f5d63a5
Lewis Prior <priorlewis43@gmail.com>
2026-02-06 16:04:02 -0600

    chore: workspace snapshot

--- .jjconflict-side-0/crates/oya-ui/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! # Oya UI
-//!
-//! This crate is a placeholder for the future Leptos-based frontend.
-//! It will be implemented in a future iteration.
-
-#![warn(missing_docs)]
-
-// Placeholder - no implementation yet
